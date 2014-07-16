66e93aa4517d7e3b42c599cd5e474b842d06b299
Konstantin Cheban <k-0-s@mail.ru>
2014-07-16 11:41:02 +0300

    Revert "Revert "return buff logic""
    This reverts commit 832df9f44089084db7c9689e842c718a954e35db.

--- src/com/example/wizard1/views/SelfGUI.java
+++ src/com/example/wizard1/views/SelfGUI.java
@@ -12,7 +12,7 @@ public class SelfGUI extends PlayerGUI {
 	public SelfGUI(Activity a, int hp, int mana) {
 		super(a);
 		playerName = (TextView) a.findViewById(R.id.self_name);
-		playerName.setText("Fix GUI #1");
+		playerName.setText("Return buff logic");
 		debugField = (TextView) a.findViewById(R.id.self_debug);
 		healthBar = (HealthIndicator) a.findViewById(R.id.self_health);
 		healthBar.setMaxValue(hp);
