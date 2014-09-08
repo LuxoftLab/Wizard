05e85e374406efbaee59d6d55562aa6223b3ae56
Berlizov <berlizov@me.com>
2014-09-08 22:01:33 +0300

    Changing the size of the acceleration data array if it is too large.

--- src/com/wizardfight/SensorAndSoundThread.java
+++ src/com/wizardfight/SensorAndSoundThread.java
@@ -110,7 +110,7 @@ public class SensorAndSoundThread extends Thread implements SensorEventListener 
 	public ArrayList<Vector3d> stopAndGetResult() {
 		listening = false;
 		soundPool.pause(wandStreamID);
-		return records;
+		return resize(records,50);
 	}
 
 	public void setSoundPlaying(boolean isPlaying) {
@@ -150,4 +150,26 @@ public class SensorAndSoundThread extends Thread implements SensorEventListener 
 			amplitude = 1.0f;
 		soundPool.setVolume(wandStreamID, amplitude, amplitude);
 	}
+    private static ArrayList<Vector3d> resize(ArrayList<Vector3d> a,int size)
+    {
+        if(a.size()<size)return a;
+        ArrayList<Vector3d> s=new ArrayList<Vector3d>();
+        double step=a.size()/(double)(size);
+        for (int i = 0; i < size; i++) {
+            s.add(getArrayResizeItem(a, step * i));
+        }
+        return s;
+    }
+    private static Vector3d getArrayResizeItem(ArrayList<Vector3d> a, double i){
+        if(((i==((int)i))))
+            return a.get((int)i);
+        if(i+1>=a.size())
+            return a.get(a.size()-1);
+        double fPart = i % 1;
+        double x= a.get((int)i).x+( a.get((int)i+1).x- a.get((int)i).x)*fPart;
+        double y= a.get((int)i).y+( a.get((int)i+1).y- a.get((int)i).y)*fPart;
+        double z= a.get((int)i).z+( a.get((int)i+1).z- a.get((int)i).z)*fPart;
+        Vector3d t=new Vector3d(x,y,z);
+        return t;
+    }
 }
